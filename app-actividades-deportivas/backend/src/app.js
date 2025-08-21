3f449cebf89943d709fcf76913c6c07c51488520
Emilio <EmilianoR117@outlook.com>
2025-08-21 14:26:24 -0700

    creacion de authcontroller, model user y routes

---
+++ app-actividades-deportivas/backend/src/app.js
@@ -0,0 +1,20 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import { config } from "dotenv"
+import authRoutes from "./routes/auth.routes.js"
+
+config()
+
+const PORT = process.env.PORT
+const app = express()
+
+app.use( express.json() )
+app.use( cors() )
+app.use( "/api", authRoutes )
+
+mongoose.connect(process.env.MONGO_KEY).then( () => console.log( "Conectado a MongoDB" ) )
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en el puerto", PORT)
+})
