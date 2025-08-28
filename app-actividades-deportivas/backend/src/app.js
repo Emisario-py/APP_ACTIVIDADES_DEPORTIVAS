8c508e7b8467e2fdbfb6ba401a94e44db2994796
Emilio <EmilianoR117@outlook.com>
2025-08-28 00:52:02 -0700

    se completo el midleware, backend terminado

--- app-actividades-deportivas/backend/src/app.js
+++ app-actividades-deportivas/backend/src/app.js
@@ -4,6 +4,7 @@ import cors from "cors"
 import { config } from "dotenv"
 import authRoutes from "./routes/auth.routes.js"
 import registersRouter from "./routes/registers.routes.js"
+import cookieParser from "cookie-parser"
 
 config()
 
@@ -11,7 +12,12 @@ const PORT = process.env.PORT
 const app = express()
 
 app.use( express.json() )
-app.use( cors() )
+app.use( cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}) )
+app.use( cookieParser() )
+
 app.use( "/api/registers", registersRouter )
 app.use( "/api", authRoutes )
 
