76374add467802ebdcb4f3c284a92f1f1997031f
Yotogle <jjtg630@gmail.com>
2025-08-31 01:39:21 -0600

    +home

--- app-actividades-deportivas/frontend/src/Home.jsx
+++ app-actividades-deportivas/frontend/src/Home.jsx
@@ -1,7 +1,7 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { TarjetaDeporte } from './Components/TarjetaDeporte'
 import { FormularioGeneral } from './Components/FormularioGeneral'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 
 
 
@@ -12,7 +12,7 @@ export const Home = () => {
       <Router>
       <Routes>
 
-        <Route path="/" element={/*<PrivateRoute>*/<TarjetaDeporte/>/*</PrivateRoute>*/} />
+        <Route path="/home" element={/*<PrivateRoute>*/<TarjetaDeporte/>/*</PrivateRoute>*/} />
         <Route path="/FormularioGeneral/:deporte" element={/*<PrivateRoute>*/<FormularioGeneral />/*</PrivateRoute>*/} />
 
       </Routes>
