44b2eddb8b4a380c2e0de1e318c9b76dd4972590
Yotogle <jjtg630@gmail.com>
2025-08-31 01:39:00 -0600

    repositorio de perfil

--- app-actividades-deportivas/frontend/src/Home.jsx
+++ app-actividades-deportivas/frontend/src/Home.jsx
@@ -1,6 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import { TarjetaDeporte } from './Components/TarjetaDeporte'
 import { FormularioGeneral } from './Components/FormularioGeneral'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
 
