1161232dfd3c872e48afcfbe7f815b131391495c
Eduardo Domínguez Padilla <113957669+eduardotec05@users.noreply.github.com>
2025-09-01 17:28:22 -0600

    estilo del login, schema mejorado e implementado

--- app-actividades-deportivas/frontend/src/App.jsx
+++ app-actividades-deportivas/frontend/src/App.jsx
@@ -1,16 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./pages/Auth";
 
-function Dashboard() {
-  return <h1>🏋️ Bienvenido al Dashboard</h1>;
-}
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Auth />} />
-        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
