ea93d6bba59e12a7a280e47ca17a9c266cc8a752
ImaFuentes10 <ima.fuentes.c@gmail.com>
2025-09-01 15:22:59 -0700

    feat: :sparkles: Añado componente inicial MetricsTable
    Creo componente metricstable para crear la tabla de totales. Cambio mock de datos

---
+++ app-actividades-deportivas/frontend/src/Components/charts/MetricsTable.jsx
@@ -0,0 +1,17 @@
+export default function MetricsTable () {
+  return (
+    <div className='bg-gray-700 p-4 rounded-2xl shadow mt-6 flex flex-col justify-around h-1/2 '>
+      <div className='bg-gray-900 rounded-2xl shadow flex justify-around p-4'>
+        <div>
+          <h2>Totales últimos 15 días</h2>
+          <p>fecha - fecha</p>
+        </div>
+        <div>
+          <h2>Deporte</h2>
+          <p>estadistica</p>
+        </div>
+
+      </div>
+    </div>
+  )
+}
--- app-actividades-deportivas/frontend/src/pages/Metrics.jsx
+++ app-actividades-deportivas/frontend/src/pages/Metrics.jsx
@@ -1,10 +1,12 @@
 import ChartContainer from '../components/charts/ChartContainer'
+import MetricsTable from '../components/charts/MetricsTable'
 
 export default function Metrics () {
   return (
     <>
       <div className='h-screen'>
         <ChartContainer />
+        <MetricsTable />
       </div>
 
     </>
