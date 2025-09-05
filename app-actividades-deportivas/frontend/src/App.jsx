2ea9b006a129f33beb7dcc63ecd8e06df63b3d13
Eduardo Domínguez Padilla <113957669+eduardotec05@users.noreply.github.com>
2025-09-04 20:12:02 -0600

    solucion a un pequeño error en las rutas

--- app-actividades-deportivas/frontend/src/App.jsx
+++ app-actividades-deportivas/frontend/src/App.jsx
@@ -16,9 +16,7 @@ import { Home } from './pages/Home.jsx'
 const AppContent = () => {
   useAuth()
   const location = useLocation()
-
-  // Agrega '/logout' a la lista de rutas sin Navbar
-  const hideNavbarRoutes = ['/login', '/', '/register', '/logout']
+  const hideNavbarRoutes = ['/login', '/', '/register']
 
   const showNavbar = !hideNavbarRoutes.includes(location.pathname)
 
@@ -28,6 +26,7 @@ const AppContent = () => {
       <main className={`${showNavbar ? 'ml-64' : 'min-h-screen bg-gray-800 text-gray-100 p-6'}`}>
         <Routes>
           {/* El componente Auth recibe la función para cambiar el estado */}
+          <Route path='/' element={<Navigate to='/login' replace />} />
           <Route path='/login' element={<Auth />} />
           <Route path='/register' element={<Register />} />
 
