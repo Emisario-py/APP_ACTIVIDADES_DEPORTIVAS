2e70975f97425361e585d53b2e2685f8947dcad3
Emiliano <EmilianoR117@outlook.com>
2025-09-24 18:13:51 -0600

    Correciones finales para versel

--- app-actividades-deportivas/backend/src/app.js
+++ app-actividades-deportivas/backend/src/app.js
@@ -13,7 +13,10 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-  origin: 'https://app-actividades-deportivas-vp5p.vercel.app/',
+  origin: [
+    "http://localhost:5173",
+    "https://1-web-d-proyecto-final-83lg.vercel.app"
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }))
